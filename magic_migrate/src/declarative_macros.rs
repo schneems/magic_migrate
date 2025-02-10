abd5af5ce52f629d2968cdd84477899566a9ca5b
Schneems <richard.schneeman+foo@gmail.com>
2025-02-10 12:54:09 -0600

    Deprecate declarative macros

--- magic_migrate/src/declarative_macros.rs
+++ magic_migrate/src/declarative_macros.rs
@@ -1,18 +1,7 @@
-/// Macro for linking structs together in an infallible [`crate::Migrate`] migration chain
-/// without defining the first migration in the chain
-/// or the deserializer.
-///
-/// i.e. it Links A => B, B => C etc. without linking A => A
-///
-/// Relies on A => A to define the type of deserializer.
-/// That means this can be reused for any deserializer you want.
-///
-/// For a fallible migration chain see [`crate::try_migrate_link`].
-///
-/// This macro is used by higher level macros like:
-///
-/// - [`crate::migrate_toml_chain!`] for TOML migrations
-/// - [`crate::migrate_deserializer_chain!`] for migrations with a custom deserializer
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate, error = std::convert::Infallible)]` macro instead"
+)]
 #[macro_export]
 macro_rules! migrate_link {
     // Base case, defines the trait
@@ -35,25 +24,10 @@ macro_rules! migrate_link {
     );
 }
 
-/// Links each struct passed in to each other to build a [`crate::Migrate`] link chain.
-/// Including creating the first "self" migration which defines the deserializer
-/// to be TOML.
-///
-/// To BYO deserializer use [`crate::migrate_deserializer_chain!`]. For a failible migration
-/// use [`crate::try_migrate_toml_chain!`].
-///
-/// ## Example
-///
-/// See [`crate`] module docs for a full example use case
-///
-/// ```no_run
-/// use magic_migrate::Migrate;
-#[doc = include_str!("fixtures/personV1_V2.txt")]
-///
-/// // - Link PersonV1 => PersonV1 and set the toml deserializer
-/// // - Link PersonV1 => PersonV2
-/// magic_migrate::migrate_toml_chain!(PersonV1, PersonV2);
-/// ```
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate, error = std::convert::Infallible)]` macro instead"
+)]
 #[macro_export(local_inner_macros)]
 macro_rules! migrate_toml_chain {
     // Base case
@@ -73,21 +47,10 @@ macro_rules! migrate_toml_chain {
     );
 }
 
-/// Macro for linking structs together in an infallible [`crate::TryMigrate`] migration chain
-/// without defining the first migration in the chain
-/// or the deserializer.
-///
-/// i.e. it Links A => B, B => C etc. without linking A => A
-///
-/// Relies on A => A to define the type of deserializer.
-/// That means this can be reused for any deserializer you want.
-///
-/// For a infallible migration chain see [`crate::migrate_link`].
-///
-/// This macro is used by higher level macros like:
-///
-/// - [`crate::try_migrate_toml_chain!`] for TOML migrations
-/// - [`crate::try_migrate_deserializer_chain!`] for migrations with a custom deserializer
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate)]` macro instead"
+)]
 #[macro_export]
 macro_rules! try_migrate_link {
     // Base case, defines the trait
@@ -111,42 +74,10 @@ macro_rules! try_migrate_link {
     );
 }
 
-/// A macro to help define [`crate::TryMigrate`] based migrations
-///
-/// To use a different deserializer use [`crate::try_migrate_deserializer_chain!`].
-/// To define infallible migrations use [`crate::migrate_toml_chain!`].
-///
-/// # Example
-///
-/// ```rust
-/// use magic_migrate::TryMigrate;
-#[doc = include_str!("fixtures/try_personV1_V2.txt")]
-///
-/// magic_migrate::try_migrate_toml_chain!(
-///     error: PersonMigrationError,
-///     chain: [PersonV1, PersonV2],
-/// );
-///
-/// // Now, given a serialized struct
-/// let toml_string = toml::to_string(&PersonV1 {
-///     name: "Schneems".to_string(),
-///     title: Some("Chief Taco Officer".to_string())
-/// })
-/// .unwrap();
-///
-/// // Cannot deserialize PersonV1 toml directly into PersonV2
-/// let result = toml::from_str::<PersonV2>(&toml_string);
-///  assert!(result.is_err());
-///
-/// // Can deserialize to PersonV1 then migrate to PersonV2
-/// let person: PersonV2 = PersonV2::try_from_str_migrations(&toml_string).unwrap().unwrap();
-/// assert_eq!(person.name, "Schneems".to_string());
-///
-/// // Conversion can fail (missing a Title)
-/// let result = PersonV2::try_from_str_migrations(&"name = 'Schneems'").unwrap();
-/// assert!(result.is_err());
-/// assert!(matches!(result, Err(PersonMigrationError::TitleCannotBeEmpty)));
-/// ```
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate)]` macro instead"
+)]
 #[macro_export]
 macro_rules! try_migrate_toml_chain {
     // Base case
@@ -171,34 +102,10 @@ macro_rules! try_migrate_toml_chain {
     );
 }
 
-/// A macro to help define infallible [`crate::Migrate`] based migrations with an arbitrary deserializer.
-///
-/// The argument passed to `deserializer:` in the macro should be a function that returns an `impl Deserializer`.
-///
-/// For a fallible migration chain see [`crate::try_migrate_deserializer_chain!`].
-///
-/// # Example
-///
-/// ```rust
-/// use magic_migrate::Migrate;
-#[doc = include_str!("fixtures/personV1_V2.txt")]
-///
-/// magic_migrate::migrate_deserializer_chain!(
-///     deserializer: toml::Deserializer::new,
-///     chain: [PersonV1, PersonV2],
-/// );
-///
-/// // Now, given a serialized struct
-/// let toml_string = toml::to_string(&PersonV1 { name: "Schneems".to_string() }).unwrap();
-///
-/// // Cannot deserialize PersonV1 toml into PersonV2
-/// let result = toml::from_str::<PersonV2>(&toml_string);
-///  assert!(result.is_err());
-///
-/// // Can deserialize to PersonV1 then migrate to PersonV2
-/// let person: PersonV2 = PersonV2::from_str_migrations(&toml_string).unwrap();
-/// assert_eq!(person.name, "Schneems".to_string());
-/// ```
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate, error = std::convert::Infallible)]` macro instead"
+)]
 #[macro_export]
 macro_rules! migrate_deserializer_chain {
     // Base case
@@ -230,47 +137,10 @@ macro_rules! migrate_deserializer_chain {
     };
 }
 
-/// A macro to help define [`crate::TryMigrate`] based migrations with an arbitrary deserializer.
-///
-/// The argument passed to `deserializer:` in the macro should be a function that returns an `impl Deserializer`.
-///
-/// The argument passed to `error:` in the macro should be the error type that every error from the [`TryFrom`] implementations
-/// can be coherced [`Into`].
-///
-/// For an infallible migration chain see [`crate::migrate_deserializer_chain!`].
-///
-/// ## Example
-///
-/// ```rust
-/// use magic_migrate::TryMigrate;
-#[doc = include_str!("fixtures/try_personV1_V2.txt")]
-///
-/// magic_migrate::try_migrate_deserializer_chain!(
-///     deserializer: toml::Deserializer::new,
-///     error: PersonMigrationError,
-///     chain: [PersonV1, PersonV2],
-/// );
-///
-/// // Now, given a serialized struct
-/// let toml_string = toml::to_string(&PersonV1 {
-///     name: "Schneems".to_string(),
-///     title: Some("Chief Taco Officer".to_string())
-/// })
-/// .unwrap();
-///
-/// // Cannot deserialize PersonV1 toml directly into PersonV2
-/// let result = toml::from_str::<PersonV2>(&toml_string);
-///  assert!(result.is_err());
-///
-/// // Can deserialize to PersonV1 then migrate to PersonV2
-/// let person: PersonV2 = PersonV2::try_from_str_migrations(&toml_string).unwrap().unwrap();
-/// assert_eq!(person.name, "Schneems".to_string());
-///
-/// // Conversion can fail (missing a Title)
-/// let result = PersonV2::try_from_str_migrations(&"name = 'Schneems'").unwrap();
-/// assert!(result.is_err());
-/// assert!(matches!(result, Err(PersonMigrationError::TitleCannotBeEmpty)));
-/// ```
+#[deprecated(
+    since = "1.1.0",
+    note = "Please use the `#[derive(TryMigrate)]` macro instead"
+)]
 #[macro_export]
 macro_rules! try_migrate_deserializer_chain {
     // Base case
